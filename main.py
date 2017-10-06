a4dc24458301a781f792b9e8908f0fb9732051e7
mrpeel <mrpeel@gmail.com>
2017-10-06 19:58:34 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,126 @@
+"""Entry point to evolving the neural network. Start here."""
+import logging
+from optimizer import Optimizer
+from tqdm import tqdm
+import numpy as np
+
+# Setup logging.
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p',
+    level=logging.DEBUG,
+    filename='log.txt'
+)
+
+def train_networks(networks):
+    """Train each network.
+
+    Args:
+        networks (list): Current population of networks
+    """
+    pbar = tqdm(total=len(networks))
+    for network in networks:
+        network.train()
+        pbar.update(1)
+    pbar.close()
+
+def get_np_losses(networks):
+    """Get the average loss for a group of networks.
+
+    Args:
+        networks (list): List of networks
+
+    Returns:
+        float: The average loss of a population of networks.
+
+    """
+    losses = []
+    for network in networks:
+        losses.append(network.loss)
+
+    np_losses = np.array(losses)
+
+    return np_losses
+
+def generate(generations, population, nn_param_choices):
+    """Generate a network with the genetic algorithm.
+
+    Args:
+        generations (int): Number of times to evole the population
+        population (int): Number of networks in each generation
+        nn_param_choices (dict): Parameter choices for networks
+
+    """
+    optimizer = Optimizer(nn_param_choices)
+    networks = optimizer.create_population(population)
+
+    # Evolve the generation.
+    for i in range(generations):
+        logging.info("***Doing generation %d of %d***" %
+                     (i + 1, generations))
+
+        print("\r***Doing generation %d of %d***" %
+                     (i + 1, generations))
+
+        # Train and get loss for networks.
+        train_networks(networks)
+
+        # Get the average loss for this generation.
+        np_loss = get_np_losses(networks)
+
+        # Print out the average loss each generation.
+        logging.info("Generation average: %.2f%%" % (np.mean(np_loss)))
+        logging.info("Generation maximum: %.2f%%" % (np.amax(np_loss)))
+        logging.info("Generation minimum: %.2f%%" % (np.amin(np_loss)))
+        logging.info('-'*80)
+
+        print("\rGeneration average: %.2f%%" % (np.mean(np_loss)))
+        print("Generation maximum: %.2f%%" % (np.amax(np_loss)))
+        print("Generation minimum: %.2f%%" % (np.amin(np_loss)))
+        print('-'*80)
+
+        # Evolve, except on the last iteration.
+        if i != generations - 1:
+            # Do the evolution.
+            networks = optimizer.evolve(networks)
+
+    # Sort our final population.
+    networks = sorted(networks, key=lambda x: x.loss, reverse=True)
+
+    # Print out the top 5 networks.
+    print_networks(networks[:5])
+
+def print_networks(networks):
+    """Print a list of networks.
+
+    Args:
+        networks (list): The population of networks
+
+    """
+    logging.info('-'*80)
+    for network in networks:
+        network.print_network()
+
+def main():
+    """Evolve a network."""
+    generations = 10  # Number of times to evole the population.
+    population = 20  # Number of networks in each generation.
+
+    nn_param_choices = {
+        'max_depth':  [30, 50, 70, 90, 110, 130, 150],
+        'base_score': [0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.0],
+        'colsample_bylevel': [0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.0],
+        'colsample_bytree': [0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.0],
+        'gamma': [0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.0],
+        'min_child_weight': [0, 1, 2, 3, 4, 5],
+        'learning_rate':  [0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175],
+        # 'booster': ['gbtree', 'dart'],
+    }
+
+    logging.info("***Evolving %d generations with population %d***" %
+                 (generations, population))
+
+    generate(generations, population, nn_param_choices)
+
+if __name__ == '__main__':
+    main()
---
+++ train.py
@@ -0,0 +1,131 @@
+"""
+Utility used by the Network class to actually train.
+
+Based on:
+    https://github.com/fchollet/keras/blob/master/examples/mnist_mlp.py
+
+"""
+import xgboost as xgb
+
+from sklearn.metrics import mean_absolute_error
+import pandas as pd
+import numpy as np
+
+import logging
+
+def sc_mean_absolute_percentage_error(y_true, y_pred):
+    diff = K.abs((y_true - y_pred) / K.clip(K.abs(y_true),
+                                            0.15,
+                                            None))
+    return 100. * K.mean(diff, axis=-1)
+
+def safe_log(input_array):
+    return_vals = input_array.copy()
+    neg_mask = return_vals < 0
+    return_vals = np.log(np.absolute(return_vals) + 1)
+    return_vals[neg_mask] *= -1.
+    return return_vals
+
+def safe_mape(actual_y, prediction_y):
+    """
+    Calculate mean absolute percentage error
+
+    Args:
+        actual_y - numpy array containing targets with shape (n_samples, n_targets)
+        prediction_y - numpy array containing predictions with shape (n_samples, n_targets)
+    """
+    diff = np.absolute((actual_y - prediction_y) / np.clip(np.absolute(actual_y), 0.25, None))
+    return 100. * np.mean(diff)
+
+def compile_model(network):
+    """Compile a sequential model.
+
+    Args:
+        network (dict): the parameters of the network
+
+    Returns:
+        a compiled network.
+
+    """
+    # Get our network parameters.
+    max_depth = network['max_depth']
+    base_score = network['base_score']
+    colsample_bylevel = network['colsample_bylevel']
+    colsample_bytree = network['colsample_bytree']
+    gamma = network['gamma']
+    learning_rate = network['learning_rate']
+    booster = network['learning_rate']
+    min_child_weight = network['min_child_weight']
+
+    model  = xgb.XGBRegressor(nthread=-1, n_estimators=5000,
+                              # booster=booster,
+                              max_depth=max_depth,
+                              base_score=base_score,
+                              colsample_bylevel=colsample_bylevel,
+                              colsample_bytree=colsample_bytree,
+                              gamma=gamma,
+                              learning_rate=learning_rate,
+                               min_child_weight=min_child_weight)
+
+    return model
+
+def train_and_score_xgb(network):
+    """Train the model, return test loss.
+
+    Args:
+        network (dict): the parameters of the network
+
+    """
+
+    df_all_train_x = pd.read_pickle('data/df_all_train_x.pkl.gz', compression='gzip')
+    df_all_train_y = pd.read_pickle('data/df_all_train_y.pkl.gz', compression='gzip')
+    df_all_train_actuals = pd.read_pickle('data/df_all_train_actuals.pkl.gz', compression='gzip')
+    df_all_test_x = pd.read_pickle('data/df_all_test_x.pkl.gz', compression='gzip')
+    df_all_test_y = pd.read_pickle('data/df_all_test_y.pkl.gz', compression='gzip')
+    df_all_test_actuals = pd.read_pickle('data/df_all_test_actuals.pkl.gz', compression='gzip')
+
+    train_y = df_all_train_y[0].values
+    train_actuals = df_all_train_actuals[0].values
+    train_log_y = safe_log(train_y)
+    train_x = df_all_train_x.as_matrix()
+    test_actuals = df_all_test_actuals.as_matrix()
+    test_y = df_all_test_y[0].values
+    test_log_y = safe_log(test_y)
+    test_x = df_all_test_x.as_matrix()
+
+
+
+    model = compile_model(network)
+
+    print('\rNetwork')
+
+    for property in network:
+        print(property, ':', network[property])
+        logging.info('%s: %s' % (property, network[property]))
+
+
+    eval_set = [(test_x, test_y)]
+    model.fit(train_x, train_y, early_stopping_rounds=5, eval_metric='mae', eval_set=eval_set,
+                verbose=False)
+
+    predictions = model.predict(test_x)
+    score = mean_absolute_error(test_y, predictions)
+
+    print('\rResults')
+
+    best_round = model.best_iteration
+
+    if np.isnan(score):
+        score = 9999
+
+    print('best round:', best_round)
+    print('loss:', score)
+    print('-' * 20)
+
+    logging.info('best round: %d' % best_round)
+    logging.info('loss: %.2f' % score)
+    logging.info('-' * 20)
+
+    return score
+
+
